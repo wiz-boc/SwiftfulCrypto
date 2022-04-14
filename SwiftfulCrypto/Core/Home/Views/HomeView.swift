02b6ad7c3151eda0ad8c64d5916eb9b978108692
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-04-13 22:06:11 -0500

    Create header section

---
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,63 @@
+//
+//  HomeView.swift
+//  SwiftfulCrypto
+//
+//  Created by wizz on 4/13/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack{
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack{
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack{
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+                .animation(.none, value: showPortfolio)
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(.theme.accent)
+                .animation(.none, value: showPortfolio)
+            
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
--- SwiftfulCrypto/SwiftfulCryptoApp.swift
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftfulCryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
