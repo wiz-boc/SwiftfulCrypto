a5ece9740cf2e97ee00c0956045dd02d9b214dbe
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-04-18 13:05:04 -0500

    Create reuseable network call

---
+++ SwiftfulCrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkingManager.swift
+//  SwiftfulCrypto
+//
+//  Created by wizz on 4/18/22.
+//
+
+import Foundation
+import Combine
+
+
+class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+                case .badURLResponse(url: let url):  return "[🔥] Bad response from URL: \(url)"
+                case .unknown: return "[⚠️] Unknown error occured"
+            }
+        }
+    }
+    static func download(url: URL) -> AnyPublisher<Data, Error>{
+        URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url)})
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    ///
+    ///Subscribers.Completion<Publishers.Decode<AnyPublisher<Data, Error>, [CoinModel], JSONDecoder>.Failure>
+    ///
+    static func handleCompletion(completion: Subscribers.Completion<Error>){
+        switch completion {
+            case .finished:
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+        }
+    }
+}
