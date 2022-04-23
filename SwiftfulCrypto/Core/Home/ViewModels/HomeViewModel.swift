c93e0e99894a6e2720d0ccbcd7890a617ce8d364
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-04-22 23:33:55 -0500

    implement Filtering using the search bar for coin data

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -20,11 +20,31 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers(){
-        dataService.$allCoins
+//        dataService.$allCoins
+//            .sink { [weak self] returnedCoins in
+//                guard let self = self else { return }
+//                self.allCoins = returnedCoins
+//            }
+//            .store(in: &cancellables)
+        
+        //updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 guard let self = self else { return }
                 self.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel]{
+        guard !text.isEmpty else { return coins }
+        
+        let lowercasedText = text.lowercased()
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) || coin.symbol.lowercased().contains(lowercasedText) || coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
