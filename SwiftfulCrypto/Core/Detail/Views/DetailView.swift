25628819ae9ddfa33f3254b7aac7c24d38f1a76a
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-05-01 13:34:07 -0500

    Add graphic to detail view

--- SwiftfulCrypto/Core/Detail/Views/DetailView.swift
+++ SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -36,21 +36,29 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView{
-            VStack(spacing: 20){
-                Text("")
-                    .frame(height: 150)
-                
-                overviewTitle
-                Divider()
-                overviewGrid
-                additionalTitle
-                Divider()
-                additionalGrid
-                
+            VStack{
+                ChartView(coin: vm.coin)
+                    .padding(.vertical)
+                VStack(spacing: 20){
+                    
+                    overviewTitle
+                    Divider()
+                    overviewGrid
+                    additionalTitle
+                    Divider()
+                    additionalGrid
+                    
+                }
+                .padding()
             }
-            .padding()
+            
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
@@ -64,6 +72,17 @@ struct DetailView_Previews: PreviewProvider {
 
 
 extension DetailView {
+    
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(.theme.secondaryText)
+            
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
     
     private var overviewTitle: some View {
         Text("Overview")
